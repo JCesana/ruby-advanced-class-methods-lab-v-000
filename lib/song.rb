47cf7ae2280686fa7c89171923a82a389ae14a50
Josh Cesana <joshuacesana@gmail.com>
2018-05-24 04:37:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,16 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+
   def self.all
     @@all
   end
 
+
   def save
     self.class.all << self
   end
+  
   
   def self.create 
     song = Song.new 
@@ -17,20 +20,24 @@ class Song
     song 
   end 
   
+  
   def self.new_by_name(song_name)
     song = Song.new 
     song.name = song_name
     song
   end 
   
+  
   def self.create_by_name(song_name)
     @name = song_name
     Song.create 
   end 
   
+  
   def self.find_by_name(song_name)
     self.all.detect { |song| song.name == song_name }
   end 
+  
   
   def self.find_or_create_by_name(song_name)
     if Song.find_by_name(song_name)
@@ -40,9 +47,11 @@ class Song
     end 
   end 
   
+  
   def self.alphabetical
     self.all.sort_by { |song| song.name }
   end 
+  
   
   def self.new_from_filename(filename)
     file_name_array = filename.split("-")
@@ -54,6 +63,7 @@ class Song
     song 
   end 
   
+  
   def self.create_from_filename(filename)
     file_name_array = filename.split("-")
     @artist_name = file_name_array[0].strip
@@ -63,20 +73,9 @@ class Song
     song 
   end 
   
+  
   def self.destroy_all
     self.all.clear
   end 
-  
+
 end
-
-
-
-
-
-
-
-
-
-
-
-
